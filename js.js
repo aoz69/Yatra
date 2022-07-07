7233c983bd3b6d547ef0fd5b93645fa456559291
aoz69 <aozpokharel@gmail.com>
2022-07-07 14:01:18 +0545

    welcome pages done in normal html

---
+++ js.js
@@ -0,0 +1,15 @@
+// function myFunction() {
+//     window.location = "create.html";
+// }
+
+function aniCh() {
+
+    var newdiv = document.getElementById("full");
+    var creatediv = document.getElementById("full2");
+    document.body.style.animation = "colorchange 0.4s forwards"
+    creatediv.style.display = "block";
+    creatediv.style.animation = "fontchange 0.4s forwards ";
+    newdiv.style.animation = "pagechange 0.4s forwards ";
+    console.log("NEWNEWNWE");
+
+}
